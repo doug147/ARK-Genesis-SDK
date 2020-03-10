1ce8a8563b65375d398f90c3763d2215c92f0797
doug147 <douglorenzen@gmail.com>
2020-03-09 23:28:04 -0400

    SDK dumped on 3/9/2020
    SDK dumped on 3/9/2020

---
+++ SDK/ARK_BP_Sky_Sphere_parameters.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+// ARK (291.0) SDK
+
+#ifdef _MSC_VER
+	#pragma pack(push, 0x8)
+#endif
+
+#include "ARK_BP_Sky_Sphere_classes.hpp"
+
+namespace sdk
+{
+//---------------------------------------------------------------------------
+//Parameters
+//---------------------------------------------------------------------------
+
+// Function BP_Sky_Sphere.BP_Sky_Sphere_C.UserConstructionScript
+struct ABP_Sky_Sphere_C_UserConstructionScript_Params
+{
+};
+
+// Function BP_Sky_Sphere.BP_Sky_Sphere_C.ExecuteUbergraph_BP_Sky_Sphere
+struct ABP_Sky_Sphere_C_ExecuteUbergraph_BP_Sky_Sphere_Params
+{
+	int                                                EntryPoint;                                               // (Parm, ZeroConstructor, IsPlainOldData)
+};
+
+}
+
+#ifdef _MSC_VER
+	#pragma pack(pop)
+#endif
